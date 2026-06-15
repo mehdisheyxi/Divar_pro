4306a122e88cbc5d88d64755a70bfbdc27a1b4ae
metti <131227781+mehdisheyxi@users.noreply.github.com>
2026-06-15 07:47:57 +0330

    V 1.0.0.0.0

--- Master.py
+++ Master.py
@@ -1,36 +1,145 @@
-import requests
-import json
-import fake_useragent
-import os
-import scraper
-import sys
 from scraper import *
-
 import sys
+import os
+
 sys.stdout.reconfigure(encoding='utf-8')
+print('WELCOME')
+iphone_models = {
 
-data = get_mobile()
-print(f"status code = {data['status']}")
-for widget in data['data'].get('list_widgets', []):
-    if widget.get('widget_type') == 'POST_ROW':
-        title = widget['data']['title']
-        price = widget['data']['middle_description_text']
-        print(f'title = {title}')
-        print(f'price = {price}')
-        print('-' * 10)
+    '1': 'iphone 17 pro max',
+    '2': 'iphone 17 pro',
+    '3': 'iphone 17',
 
-print('-*-*' * 50)
-print('-*-*' * 50)
-print('-*-*' * 50)
+    '4': 'iphone 16 pro max',
+    '5': 'iphone 16 pro',
+    '6': 'iphone 16',
 
-data2 = get_homepage()
-print(f"status code = {data2['status']}")
-for widget in data2['data'].get('list_widgets', []):
-    if widget.get('widget_type') == 'POST_ROW':
-        title = widget['data'].get('title')
-        print(f'title = {title}')
-        price = widget['data'].get('middle_description_text')
-        if price:
-            print(f'price = {price}')
+    '7': 'iphone 15 pro max',
+    '8': 'iphone 15 pro',
+    '9': 'iphone 15',
 
-        print('-' * 10)
+    '10': 'iphone 14 pro max',
+    '11': 'iphone 14 pro',
+    '12': 'iphone 14',
+
+    '13': 'iphone 13 pro max',
+    '14': 'iphone 13 pro',
+    '15': 'iphone 13',
+
+    '16': 'iphone 12 pro max',
+    '17': 'iphone 12 pro',
+    '18': 'iphone 12',
+
+    '19': 'iphone 11 pro max',
+    '20': 'iphone 11 pro',
+    '21': 'iphone 11',
+
+    '22': 'iphone xs max',
+    '23': 'iphone xs',
+    '24': 'iphone xr',
+
+    '25': 'iphone x',
+
+    '26': 'iphone 8 plus',
+    '27': 'iphone 8',
+
+    '28': 'iphone 7 plus',
+    '29': 'iphone 7',
+
+    '30': 'iphone 6s plus',
+    '31': 'iphone 6s',
+
+    '32': 'iphone 6 plus',
+    '33': 'iphone 6',
+
+    '34': 'iphone 5s',
+    '35': 'iphone 5',
+}
+while True:
+    os.system('cls')
+    number = input('''
+        1_Get mobile home page
+        2_Get home page
+        3_Get iphone home page
+        4_Get iphone with FILTER
+    ''')
+    if number == '1':
+        posts = get_mobile_posts()
+        for post in posts:
+            print('-' * 50)
+            print(f"title:{post['title']}")
+            print(f"price:{post['price']}")
+        input('Enter any key to exit...')
+    elif number == '2':
+        posts = get_home_posts()
+        for post in posts:
+            print('-' * 50)
+            print(f"title:{post['title']}")
+            print(f"price:{post['price']}")
+        input('Enter any key to exit...')
+    elif number == '3':
+        posts = get_iphone_posts()
+        for post in posts:
+            print('-' * 50)
+            print(f"title:{post['title']}")
+            print(f"price:{post['price']}")
+        input('Enter any key to exit...')
+    elif number == '4':
+        print('''
+            1_apple iphone 17 pro max
+            2_apple iphone 17 pro
+            3_apple iphone 17
+
+            4_apple iphone 16 pro max
+            5_apple iphone 16 pro
+            6_apple iphone 16
+
+            7_apple iphone 15 pro max
+            8_apple iphone 15 pro
+            9_apple iphone 15
+
+            10_apple iphone 14 pro max
+            11_apple iphone 14 pro
+            12_apple iphone 14
+
+            13_apple iphone 13 pro max
+            14_apple iphone 13 pro
+            15_apple iphone 13
+
+            16_apple iphone 12 pro max
+            17_apple iphone 12 pro
+            18_apple iphone 12
+
+            19_apple iphone 11 pro max
+            20_apple iphone 11 pro
+            21_apple iphone 11
+
+            22_apple iphone xs max
+            23_apple iphone xs
+            24_apple iphone xr
+
+            25_apple iphone x
+
+            26_apple iphone 8 plus
+            27_apple iphone 8
+
+            28_apple iphone 7 plus
+            29_apple iphone 7
+
+            30_apple iphone 6s plus
+            31_apple iphone 6s
+
+            32_apple iphone 6 plus
+            33_apple iphone 6
+
+            34_apple iphone 5s
+            35_apple iphone 5
+            ''')
+        a = input('Enter number:::')
+        model = iphone_models.get(a)
+        posts = get_iphone_coustoms_posts(model)
+        for post in posts:
+            print('-' * 50)
+            print(f"title:{post['title']}")
+            print(f"price:{post['price']}")
+        input('Enter any key to exit...')
--- scraper.py
+++ scraper.py
@@ -5,7 +5,13 @@ import fake_useragent
 url = 'https://api.divar.ir/v8/postlist/w/search'
 headers = {'User-Agent': fake_useragent.UserAgent().random}
 
+
 def get_mobile():
+    '''
+
+    :return:
+    '''
+
     payload = {
         f"city_ids": ["10"],
         "source_view": "CATEGORY",
@@ -49,6 +55,9 @@ def get_mobile():
         'data': response.json(),
         'status': response.status_code,
     }
+
+
+# ----------------------
 def get_homepage():
     payload2 = {
         "city_ids": [
@@ -78,5 +87,183 @@ def get_homepage():
         'data': response.json(),
         'status': response.status_code,
     }
+
+
+# ----------------------
+def get_iphone():
+    payload = {
+        "city_ids": ["10"],
+
+        "search_data": {
+            "form_data": {
+                "data": {
+
+                    "category": {
+                        "str": {
+                            "value": "mobile-phones"
+                        }
+                    },
+
+                    "brand_model": {
+                        "repeated_string": {
+                            "value": ["apple"]
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+
+    return {
+        'data': response.json(),
+        'status': response.status_code,
+    }
+
+
+def get_iphone_coustoms(model):
+    payload = {
+
+        "city_ids": ["10"],
+
+        "source_view": "FILTER",
+
+        "pagination_data": {
+            "@type": "type.googleapis.com/post_list.PaginationData",
+            "page": 1,
+            "layer_page": 1,
+        },
+
+        "disable_recommendation": False,
+
+        "map_state": {
+            "camera_info": {
+                "bbox": {}
+            }
+        },
+
+        "search_data": {
+
+            "form_data": {
+                "data": {
+
+                    "category": {
+                        "str": {
+                            "value": "mobile-phones"
+                        }
+                    },
+
+                    "brand_model": {
+                        "repeated_string": {
+                            "value": [f"apple {model}"]
+                        }
+                    }
+
+                }
+            },
+
+            "server_payload": {
+                "@type": "type.googleapis.com/widgets.SearchData.ServerPayload",
+
+                "additional_form_data": {
+                    "data": {
+
+                        "sort": {
+                            "str": {
+                                "value": "recommended"
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+
+    return {
+        'data': response.json(),
+        'status': response.status_code,
+    }
+
+
+def get_mobile_posts():
+    data = get_mobile()
+
+    posts = []  # لیست خروجی
+
+    for widget in data['data'].get('list_widgets', []):
+        if widget.get('widget_type') == 'POST_ROW':
+            title = widget['data'].get('title')
+            price = widget['data'].get('middle_description_text')
+
+            posts.append({
+                "title": title,
+                "price": price
+            })
+
+    return posts
+
+
+def get_home_posts():
+    data = get_homepage()
+    posts = []
+    for widget in data['data'].get('list_widgets', []):
+        if widget.get('widget_type') == 'POST_ROW':
+            title = widget['data'].get('title')
+            price = widget['data'].get('middle_description_text')
+            if price:
+                posts.append({
+                    "title": title,
+                    "price": price
+                })
+            else:
+                posts.append({
+                    "title": title,
+                    "price": 'NONE'
+                })
+    return posts
+
+
+def get_iphone_posts():
+    data = get_iphone()
+
+    posts = []
+
+    for widget in data['data'].get('list_widgets', []):
+
+        if widget.get('widget_type') == 'POST_ROW':
+            title = widget['data'].get('title')
+            price = widget['data'].get('middle_description_text')
+
+            posts.append({
+                "title": title,
+                "price": price
+            })
+
+    return posts
+
+
+def get_iphone_coustoms_posts(model):
+    data = get_iphone_coustoms(model)
+    print(data['status'])
+
+    posts = []
+
+    for widget in data['data'].get('list_widgets', []):
+
+        if widget.get('widget_type') == 'POST_ROW':
+            title = widget['data'].get('title')
+            price = widget['data'].get('middle_description_text')
+
+            posts.append({
+                "title": title,
+                "price": price
+            })
+    return posts
+
 
 print(url)
